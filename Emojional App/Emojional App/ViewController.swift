d5adf15b45483b68e51b4e5cb965a51b46ef627d
Apple <apple@Apples-MacBook-Pro-24.local>
2019-08-08 16:26:28 -0400

    saved

--- Emojional App/Emojional App/ViewController.swift
+++ Emojional App/Emojional App/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showMessage(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Need to Party?", message: "Go live it up!", preferredStyle: UIAlertController.Style.alert)
+    alertController.addAction(UIAlertAction(title: "OK!", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
 
-
+    
+    @IBAction func showMessage2(_ sender: UIButton) {
+    let alertController = UIAlertController(title: "Tired and Sleepy?", message: "I feel ya", preferredStyle: UIAlertController.Style.alert)
+    alertController.addAction(UIAlertAction(title: "Alright!", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func showMessage3(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Feeling a little wierd?", message: "Go delight someone with your presence!", preferredStyle: UIAlertController.Style.alert)
+    alertController.addAction(UIAlertAction(title: "Okay!", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func showMessage4(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Feeling sick?", message: "Hope you feel better soon!!", preferredStyle: UIAlertController.Style.alert)
+    alertController.addAction(UIAlertAction(title: "Yeah", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
